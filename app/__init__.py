5b3111bf059e5c3d2f36f0c4096441a11e083c05
oBrunoz <alencarb98@gmail.com>
2023-05-19 21:28:18 -0300

    Some changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,21 @@
-from flask import Flask, redirect, request, url_for
+from flask import Flask
 from app.routes.admin.adm import admin_blueprints
-from app.routes.admin import SecureModelView
+# from app.routes.admin import SecureModelView
 from app.db.db import database
 from app.models.User import User
 from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate
-from flask_login import LoginManager, current_user
+from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.register_blueprint(admin_blueprints)
 adminPage = Admin(app)
 
 from app.models.Admin import Administrator
-adminPage.add_view(SecureModelView(Administrator, database.session))
-adminPage.add_view(SecureModelView(User, database.session))
+# adminPage.add_view(SecureModelView(Administrator, database.session))
+# adminPage.add_view(SecureModelView(User, database.session))
 
 bcrypt = Bcrypt(app)
 
